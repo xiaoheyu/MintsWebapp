6c1e2a91c4f3f3363552fabd3b6a84ede9c62843
yu xiaohe <xiaohe.yu86@gmail.com>
2018-09-21 00:24:53 -0500

    The original push

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+const mongodb_conn_module = require('./mongodbConnModule');
+var db = mongodb_conn_module.connect();
+
+var Post = require("../models/post");
+
+app.get('/row', (req, res) => {
+  Post.find({"date.utc":"2018-07-28T10:00:00.000Z","parameter":"pm25","country":"US","coordinates.latitude":{$exists:true}},"country city date.utc coordinates value parameter", function (error, po) {
+	  if (error) { console.error(error); }
+	  res.send({
+			post: po
+		})
+	}).sort({_id:-1})
+})
+
+app.listen(process.env.PORT || 8081)
